27f488c00bd80d63c3ef93dfa8014ada9e5e0880
Soonhong Park <168538598+hi-shp@users.noreply.github.com>
2024-09-05 10:55:20 +0900

    Update main_specific()

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ KEYWORD_CATEGORIES = {
     "장학사업": "장학금",
     "독후감": "[공모전] 문학/수기/에세이",
     "에세이": "[공모전] 문학/수기/에세이",
+    "글쓰기": "[공모전] 문학/수기/에세이",
     "아이디어": "[공모전] 아이디어/기획",
     "튜티": "교육/특강/프로그램",
     "튜터": "봉사활동",
@@ -40,7 +41,8 @@ KEYWORD_CATEGORIES = {
     "세미나": "교육/특강/프로그램",
     "포럼": "교육/특강/프로그램",
     "컨퍼런스": "교육/특강/프로그램",
-    "지원프로그램": "교육/특강/프로그램"
+    "지원프로그램": "교육/특강/프로그램",
+    "페스티벌": "교육/특강/프로그램"
 }  # 지속적으로 추가 예정
 
 # 제외할 키워드
@@ -143,5 +145,59 @@ def main():
     # 오늘 올린 공지가 아닌 모든 게시글의 공지글 체크 해제 및 별 제거
     writenoticeService.remove_stars_and_uncheck_notices(course_url, today_date)
 
+def main_specific(url, course_url):
+    announcements = []
+    writenoticeService = WriteNoticeService()
+
+    # 메모리 누수를 방지하기 위해 주기적으로 가비지 컬렉션 실행
+    gc.collect()
+
+    # 제목+내용만 부분적으로 크롤링
+    partial_ann = crawl_ann_partial(url)
+    if partial_ann:
+        # 제목 중복 체크
+        duplicate_check = is_recent_title_duplicate(partial_ann.title)
+        print(f"중복 체크 결과: {duplicate_check} - {partial_ann.title}")
+        if duplicate_check == "중복":
+            print("")
+            return
+
+        # 키워드 기반 카테고리 분류 시도
+        category = categorize_announcement(partial_ann.title, partial_ann.content_text)
+        print(f"카테고리 분류 결과: {category}")
+
+        if category in [
+            "교육/특강/프로그램",
+            "[공모전] 공학/IT/SW",
+            "[공모전] 아이디어/기획",
+            "[공모전] 미술/디자인/건축",
+            "[공모전] 문학/수기/에세이",
+            "[공모전] 기타",
+            "장학금",
+            "봉사활동",
+            "서포터즈",
+            "취업 정보"
+        ]:
+            # 전체 공지사항 크롤링
+            full_ann = crawl_ann(url)
+            full_ann.notice_board_name = category  # 게시판 이름 업데이트
+            announcements.append(full_ann)
+            save_title(partial_ann.title)  # 제목 저장
+
+            writenoticeService.write_notices(course_url, [full_ann])  # 공지사항 작성
+            print(f"게시글 작성 완료\n")
+        else:
+            print("")
+    else:
+        print("공지사항 크롤링에 실패했습니다.")
+
 if __name__ == "__main__":
-    main()
+    use_specific = True  # False로 변경하면 main() 함수가 실행됨
+
+    if use_specific:
+        url = input("URL을 입력하세요: ")
+        course_url = "https://plato.pusan.ac.kr/course/view.php?id=157301"
+        main_specific(url, course_url)
+    else:
+        main()
+
